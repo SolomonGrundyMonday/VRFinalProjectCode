975cf0eab29047846f566ac2df25e26bfec28108
SolomonGrundyMonday <60071145+SolomonGrundyMonday@users.noreply.github.com>
2021-12-06 20:42:55 -0700

    Initial mouse-look implementation
    Has issues with movement if camera is not pointed straight ahead.

--- Navigation.cs
+++ Navigation.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Navigation : MonoBehaviour
 {
     // Private variables for movement speed, camera rig.
+    private const float mouseSensitivityX = 15.0f;
+    private const float mouseSensitivityY = 15.0f;
     private const float moveSpeed = 0.1f;
     private GameObject rig;
 
@@ -62,6 +64,50 @@ public class Navigation : MonoBehaviour
             // Set Camera rig new position based on computed coordinates.
             Vector3 newPos = new Vector3(x, rig.transform.position.y, z);
             rig.transform.SetPositionAndRotation(newPos, rig.transform.rotation);
+        }
+
+        // Mouse movement loosely based on code at: https://answers.unity.com/questions/29741/mouse-look-script.html
+        // Get mouse movement along x, y axes.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
+
+        // If mouse movement along both x, y axes.
+        if (mouseX != 0 && mouseY != 0)
+        {
+            // Limit x-axis angle to +/- 360 degrees and y-axis angle to +/- 60 degrees.
+            mouseX %= 360;
+            mouseY %= 60;
+
+            // Compute x-axis quaternion based on up vector and y-axis quaternion based on left vector.
+            Quaternion xQuaternion = Quaternion.AngleAxis(mouseX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(mouseY, Vector3.left);
+
+            // Set new rotation by multiplying original rotation quaternion by xQuaternion then yQuaternion.
+            rig.transform.SetPositionAndRotation(rig.transform.position, rig.transform.rotation * xQuaternion * yQuaternion);
+        }
+        // If mouse movement along x-axis only.
+        else if (mouseX != 0 && mouseY == 0)
+        {
+            // Limit x-axis angle to +/- 360 degrees.
+            mouseX %= 360;
+
+            // Compute x-axis quaternion based on up vector.
+            Quaternion xQuaternion = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+            // Set new rotation by multiplying original rotation quaternion by xQuaternion.
+            rig.transform.SetPositionAndRotation(rig.transform.position, rig.transform.rotation * xQuaternion);
+        }
+        // If mouse movement along y-axis only.
+        else if (mouseX == 0 && mouseY != 0)
+        {
+            // Limit y-axis angle to +/- 60 degrees.
+            mouseY %= 60;
+
+            // Compute yQuaternion based on left vector.
+            Quaternion yQuaternion = Quaternion.AngleAxis(mouseY, Vector3.left);
+
+            // Set new rotation by multiplying original rotation quaternion by yQuaternion.
+            rig.transform.SetPositionAndRotation(rig.transform.position, rig.transform.rotation * yQuaternion);
         }
     }
 }
